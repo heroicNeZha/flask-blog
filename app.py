71e5df5946095162561b33519f6a5bf4c8eada00
lzw <li495469217@163.com>
2019-01-11 15:25:31 +0800

    通过flask_script启动服务器

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
+from flask import abort
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    if name == "":
+        abort(404)
+    else:
+        return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+# 通过flask script启动
+# from flask_script import Manager
+#
+# manager = Manager(app)
+#
+# if __name__ == '__main__':
+#     manager.run()
