2c063e0168e5663551fe9c120d4e5d46e6e1a7f4
lzw <li495469217@163.com>
2019-01-18 14:19:50 +0800

    flask-form的使用 404和500的错误页面

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template
-from flask import abort
+from flask import Flask, render_template, abort, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('你叫啥名?', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello World!</h1>'
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('user'))
+    return render_template('index.html', form=form)
 
-comments = ['xixi','haha','lala','kuku']
 
-@app.route('/user/<name>')
-def user(name):
-    if name == "":
-        abort(404)
-    else:
-        return render_template('user.html', name=name, comments = comments)
+@app.route('/user')
+def user():
+        return render_template('user.html')
 
 
 if __name__ == '__main__':
