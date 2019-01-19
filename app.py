9f38141c22fbbf28814bfcfcfce50dca8aca70c0
lzw <li495469217@163.com>
2019-01-19 19:22:01 +0800

    配置sqlite数据库

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-from flask import Flask, render_template, abort, session, redirect, url_for
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+# 数据库配置
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    users = db.relationship('User' , backref = 'role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.name
 
 class NameForm(FlaskForm):
     name = StringField('你叫啥名?', validators=[DataRequired()])
@@ -31,6 +58,8 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        if session.get('name') is not None and session['name'] != form.name.data:
+            flash('你的名字改变啦！')
         session['name'] = form.name.data
         return redirect(url_for('user'))
     return render_template('index.html', form=form)
@@ -38,7 +67,7 @@ def index():
 
 @app.route('/user')
 def user():
-        return render_template('user.html')
+    return render_template('user.html')
 
 
 if __name__ == '__main__':
