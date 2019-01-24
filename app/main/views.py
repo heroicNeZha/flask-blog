b1b2fa88135b295b1fe7aa62264eb8f36bd50262
lzw <li495469217@163.com>
2019-01-24 10:15:41 +0800

    大型项目架构

---
+++ app/main/views.py
@@ -0,0 +1,28 @@
+from flask import render_template, session, redirect, url_for
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.name.data).first()
+        if user is None:
+            user = User(name=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('main.user'))
+    return render_template('index.html', form=form)
+
+
+@main.route('/user')
+def user():
+    return render_template('user.html')
---
+++ manage.py
@@ -0,0 +1,21 @@
+import os
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User, Role
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+manager.add_command('db', MigrateCommand)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
