c103d43cc5cfaa42b5022e181def1a6ba10e30de
lzw <li495469217@163.com>
2019-01-31 14:22:13 +0800

    登录成功

--- app/models.py
+++ app/models.py
@@ -1,4 +1,12 @@
-from . import  db
+from . import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -10,11 +18,24 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True, index=True)
+    name = db.Column(db.String, unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String)
+
+    @property
+    def password(self):
+        raise AttributeError("密码不可读")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.name
