dc7c4c86571d026f2dcf8be0dd552fc2fda4a379
lzw <li495469217@163.com>
2019-02-11 21:51:39 +0800

    管理员权限存在问题

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+
+from app.models import User, Role
 
 
 class EditProfileForm(FlaskForm):
@@ -8,3 +10,27 @@ class EditProfileForm(FlaskForm):
     location = StringField('所在地', validators=[Length(0, 64)])
     about_me = TextAreaField('自我介绍')
     submit = SubmitField('提交')
+
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('邮箱', validators=[DataRequired(), Length(0, 64), Email()])
+    username = StringField('用户名', validators=[DataRequired(), Length(0, 64)])
+    confirmed = BooleanField('邮件确认')
+    role = SelectField('角色', coerce=int)
+    name = StringField('姓名', validators=[Length(0, 64)])
+    location = StringField('所在地', validators=[Length(0, 64)])
+    about_me = TextAreaField('自我介绍')
+    submit = SubmitField('提交')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经注册')
+
+    def validate_name(self, field):
+        if field.data != self.user.username and User.query.filter_by(name=field.data).first():
+            raise ValidationError('用户名已存在')
--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,8 @@ from app import db
 from . import main
 from ..decorators import admin_required, permission_required
 from flask_login import login_required, current_user
-from ..models import Permission, User
-from .forms import EditProfileForm
+from ..models import Permission, User, Role
+from .forms import EditProfileForm, EditProfileAdminForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -37,6 +37,34 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('简介更新成功.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
 
 
 @main.route('/admin')
